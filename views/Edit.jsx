0b7cd06d59532ced26101c40177ebd39f115b6d5
Kevin Mathews <kev.math@yahoo.com>
2021-01-24 19:30:19 -0800

    few css changes

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -5,29 +5,30 @@ const Header = require('./Header');
 const Button = require('./components/Button');
 class Edit extends React.Component {
     render() {
-        return(
-        <Layout>
-            <Header>
+        return (
+            <Layout>
+                <Header>
 
-            </Header>
-            <div className='showPlaqueDiv2'>
-                <img className='showPlaqueImage2' src='https://i.imgur.com/5L2WhPT.png>'></img>
-            </div>
-            <div className='editPageText'>
-                <h1>Edit <a className='productN' href={`/store/${this.props.item._id}`}>{this.props.item.name}</a></h1>
-            </div>
-            <div className='newForm'>
-             <form action={`/store/${this.props.item._id}?_method=PUT`} method="POST">
-                Name: <input className='inputField' type="text" name="name"  value={this.props.item.name}/><br/>
-                Description: <input className='inputField' type="text" name="description" value={this.props.item.description}/><br/>
-                Image URL: <input className='inputField' type="text" name="image"value={this.props.item.image} /><br/>
-                Price: <input className='inputField' type="text" name="price" value={this.props.item.price}/><br/>
-                Quantity: <input className='inputField' type="text" name="quantity" value={this.props.item.quantity}/><br/>
-                <input className='editButton3' type="submit" name="" value={`Edit ${this.props.item.name}`}/>
-            </form>
-            </div>
+                </Header>
+                <div className='showPlaqueDiv3'>
+                    <img className='showPlaqueImage2' src='https://i.imgur.com/5L2WhPT.png>'></img>
+                </div>
+                <div className='editPageText'>
+                    <h1>Edit <a className='productN' href={`/store/${this.props.item._id}`}>{this.props.item.name}</a></h1>
+                </div>
+                <div className='newForm'>
+                    <form action={`/store/${this.props.item._id}?_method=PUT`} method="POST">
+                        Name: <input className='inputField' type="text" name="name" value={this.props.item.name} /><br />
+                Description: <input className='inputField' type="text" name="description" value={this.props.item.description} /><br />
+                Image URL: <input className='inputField' type="text" name="image" value={this.props.item.image} /><br />
+                Price: <input className='inputField' type="text" name="price" value={this.props.item.price} /><br />
+                Quantity: <input className='inputField' type="text" name="quantity" value={this.props.item.quantity} /><br />
+                        <input className='editButton3' type="submit" name="" value={`Edit ${this.props.item.name}`} />
+                    </form>
+                </div>
 
-</Layout>);
-}}
+            </Layout>);
+    }
+}
 
 module.exports = Edit;
--- views/New.jsx
+++ views/New.jsx
@@ -4,31 +4,31 @@ const Layout = require('./Layout');
 const Header = require('./Header');
 const Button = require('./components/Button');
 class New extends React.Component {
-    render() {
-      return (
-          <Layout>
-            <Header>
+  render() {
+    return (
+      <Layout>
+        <Header>
 
-            </Header>
-              <div className='showPlaqueDiv2'>
-              <img className='showPlaqueImage2' src='https://i.imgur.com/5L2WhPT.png>'></img>
-              </div>
-              <div className='newPageText'>
-                <h1>Add a New Toy</h1>
-              </div>
-              <div className='newForm'>
-              <form action="/store" method="POST">
-                  Name: <input className='inputField' type="text" name="name" /><br/>
-                  Description: <input className='inputField' type="text" name="description" /><br/>
-                  Image URL: <input className='inputField' type="text" name="image" /><br/>
-                  Price: <input className='inputField' type="text" name="price" /><br/>
-                  Quantity: <input className='inputField' type="text" name="quantity" /><br/>
-                  <input className='createToy' type="submit" name="" value="Create A New Toy"></input>
-                 
-               </form>
-               </div>
-          </Layout>);
-        }
+        </Header>
+        <div className='showPlaqueDiv3'>
+          <img className='showPlaqueImage2' src='https://i.imgur.com/5L2WhPT.png>'></img>
+        </div>
+        <div className='newPageText'>
+          <h1>Add a New Toy</h1>
+        </div>
+        <div className='newForm'>
+          <form action="/store" method="POST">
+            Name: <input className='inputField' type="text" name="name" /><br />
+                  Description: <input className='inputField' type="text" name="description" /><br />
+                  Image URL: <input className='inputField' type="text" name="image" /><br />
+                  Price: <input className='inputField' type="text" name="price" /><br />
+                  Quantity: <input className='inputField' type="text" name="quantity" /><br />
+            <input className='createToy' type="submit" name="" value="Create A New Toy"></input>
+
+          </form>
+        </div>
+      </Layout>);
+  }
 }
 
 module.exports = New;
